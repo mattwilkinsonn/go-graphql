cbdd2f93acaa1fb3ca96b815cf1c5c86e056b9a9
Matt Wilkinson <mattwilki17@gmail.com>
2021-02-11 17:58:42 -0500

    experimenting with raw sql

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/Zireael13/go-graphql/pkg/graph"
-	"github.com/Zireael13/go-graphql/pkg/graph/generated"
+	"github.com/Zireael13/go-graphql/graph"
+	"github.com/Zireael13/go-graphql/graph/generated"
+	"github.com/Zireael13/go-graphql/internal/db"
 )
 
 const defaultPort = "8080"
@@ -18,6 +21,18 @@ func main() {
 	if port == "" {
 		port = defaultPort
 	}
+
+	sequel, _ := sql.Open("pgx", "postgresql://postgres:postgres@localhost:5432/gograph")
+
+	base := db.New(sequel)
+
+	user := &db.CreateUserParams{
+		Username: "matt",
+		Email:    "mattwilki17@gmail.com",
+		Password: "hunter2",
+	}
+
+	base.CreateUser(context.TODO(), *user)
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
